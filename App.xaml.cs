f82ac7c6e4385b2e8eb58c774601814704642cc6
Skliarova.D.A <Skliarova.D.A@PC404-07>
2025-03-06 11:36:13 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SklyarovaDA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
